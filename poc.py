cb20574aadc469c6d1527ddb5a3cc69df531cbfd
Evin Revello <Evin@EvinRevello.com>
2015-10-23 23:42:30 -0700

    one small step...

--- poc.py
+++ poc.py
@@ -33,15 +33,93 @@ def handshake():
 	#01 60 AA 0F 00 00 0A 07 02 30 30 30 34 33 65 30 32 65 64 64 65 63 03 AB
 	
 	write('AA 02 00 00 09 06 0F 00 AB')
-	#should return with a different value, 
-	#This is assumed to be the manifest of what data is currently contained on the device
+	#This is assumed to be the manifest of data, ie what is currently contained on the device
+		#When no data is present, ie the watch has just been sitting there. Expect
+		#01 60 AA 05 00 00 09 07 02 1C 0B 39 00 AB
 	#TODO: Determine what this string is and how it is used 
+
+	#this is based on quick and constant syncs, verify as normal behavior
+	write('AA 02 00 00 07 06 0D 00 AB')
+#Same A
+	#Assumed to be tied to the 'firmware update', as when that gets pushed the contents of this change in the same spot.
+	# Three char sets change on these over the course of the contant syncs
+	# Lots of padding on this one
+	#TODO: Determine what this string is and how it is used 
+
+	write('AA 23 00 00 05 04 00 52 BC 52 B9 3C 09 12 1B 64 12 CD 9B FF FF FF FF FF FF FF FF FF FF FF FF FF FF FF FF FF FF FF FF 5E 18 AB')
+	#TODO: Determine if this string is consistant
+	#Expect
+	#01 60 AA 03 00 00 05 05 01 0B 00 AB
+
+	write('AA 02 00 00 06 04 0A 00 AB')
+	#Expect
+	#01 60 AA 03 00 00 06 05 01 0C 00 AB
+
+	write('AA 02 00 00 07 06 0D 00 AB')
+#Same A	
+
+	write('AA 08 00 00 04 04 1F 01 AC 2A 00 03 03 D8 00 AB')
+	#expect
+	#01 60 AA 03 00 00 04 05 01 0A 00 AB
+
+# Current time gets sent here
+#dynamic
+# TODO: Determine how to send specific date times
+	write('AA 08 00 00 00 04 45 9B 05 09 5C FE 4C 02 AB') #201510181406
+	#expect
+	#01 60 AA 03 00 00 00 05 01 06 00 AB
+
+	write('AA 07 00 00 0C 04 00 10 27 00 00 47 00 AB')
+	#expect
+	#01 60 AA 03 00 00 0C 05 01 12 00 AB
+
+	write('AA 02 00 00 10 04 14 00 AB')
+	#expect
+	#01 60 AA 03 00 00 10 05 01 16 00 AB
+
+	write('AA 02 00 00 01 06 07 00 AB')
+	#Expect
+	#01 60 AA 07 00 00 01 07 02 7E 0B 00 00 93 00 AB	
+	#01 60 AA 07 00 00 01 07 02 0A 00 00 00 14 00 AB	
+	#01 60 AA 07 00 00 01 07 02 14 00 00 00 1E 00 AB	
+	#01 60 AA 07 00 00 01 07 02 0A 00 00 00 14 00 AB	
+	#01 60 AA 07 00 00 01 07 02 0A 00 00 00 14 00 AB	
+	#01 60 AA 07 00 00 01 07 02 0A 00 00 00 14 00 AB	
+	#01 60 AA 07 00 00 01 07 02 0A 00 00 00 14 00 AB																			
+	
+	write('AA 02 00 00 02 06 08 00 AB')
+	#expect
+	#01 60 AA 05 00 00 02 07 02 01 00 0C 00 AB
+
+	write('AA 04 00 00 03 06 00 00 09 00 AB')
+	#expect 
+#real data here, with what appears to be aggregates in the header
+
+	write('AA 02 00 00 01 04 05 00 AB')
+	#expect
+	#01 60 AA 03 00 00 01 05 01 07 00 AB
+
+	write('')
 
 
 def chilling():
 	isChilling = read()
 	if isChilling == '01 60 AA 07 00 00 00 03 01 3D 02 06 00 49 00 AB':
 		print "device is ready for data transfer"
+
+def deletingData():
+	write('AA 02 00 00 08 06 0E 00 AB')
+	print "are we done transfering data?"
+
+	isDeletingData = read()
+
+	if isDeletingData == '01 60 AA 04 00 00 08 07 02 01 12 00 AB':
+		print "device is still deleting data from memory"
+	elif isDeletingData == '01 60 AA 04 00 00 08 07 02 00 11 00 AB':
+		print "device is done deleting data from memory"
+	else:
+		print "something unexpected happened"
+	#at this point steady chilling is what happens every so many seconds	
 
 #TODO: define the gathering of all of the possible data sets being extracted
 #Biometrics
